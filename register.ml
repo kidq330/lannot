688a2dfee067e548c260e8232f2d745991e3f1e4
Mickaël Delahaye <mickael.delahaye@gmail.com>
2013-12-14 12:36:46 +0100

    Fix useLogicalOperators line in Genlabels

--- register.ml
+++ register.ml
@@ -30,48 +30,48 @@ module Make (ST : Slicing.Type) = struct
     (* Utils.mkdir (Config.result_dir());*)
     let alarms = get_alarms() in
 
-	List.iter (fun x ->
-	  Options.Self.debug ~level:1 "alarm l.%i" (Utils.get_stmt_loc_int x)
-	) alarms;
+        List.iter (fun x ->
+          Options.Self.debug ~level:1 "alarm l.%i" (Utils.get_stmt_loc_int x)
+        ) alarms;
 
 
-	(******************************************)
-	(* SLICING & DYNAMIC ANALYSIS             *)
-	(******************************************)
+        (******************************************)
+        (* SLICING & DYNAMIC ANALYSIS             *)
+        (******************************************)
 
-	let _verdicts = ST.process alarms in
+        let _verdicts = ST.process alarms in
 
     Options.Self.feedback "finished";
 end
 
 
 (* ENTRY POINT *)
-let run() =
+let run () =
+  Cil.set_useLogicalOperators true;
   try
     if Options.MultiCond.get() then
       begin
-	let module M = Make (Slicing.Multi ) in
-	  M.run()
+        let module M = Make (Slicing.Multi ) in
+          M.run()
       end
     else
       begin
-	let module M = Make (Slicing.None ) in
-	  M.run()
+        let module M = Make (Slicing.None ) in
+          M.run()
       end
   with
-    | Globals.No_such_entry_point _ ->
-	Options.Self.feedback "`-main` parameter missing"
-    | Dynamic.Unbound_value(s) -> Options.Self.feedback "%s unbound" s
-    | Dynamic.Incompatible_type(s) -> Options.Self.feedback "%s incompatible" s
-    | Config.NoInputFile -> Options.Self.feedback "no input file"
-    | AlarmsSameLine ->
-	Options.Self.feedback "only 1 alarm per line for correct results"
-    | Failure s -> Options.Self.feedback "failure: %s" s
-    | e -> Options.Self.feedback "exception: %s" (Printexc.to_string e)
+  | Globals.No_such_entry_point _ ->
+      Options.Self.feedback "`-main` parameter missing"
+  | Dynamic.Unbound_value(s) -> Options.Self.feedback "%s unbound" s
+  | Dynamic.Incompatible_type(s) -> Options.Self.feedback "%s incompatible" s
+  | Config.NoInputFile -> Options.Self.feedback "no input file"
+  | AlarmsSameLine ->
+      Options.Self.feedback "only 1 alarm per line for correct results"
+  | Failure s -> Options.Self.feedback "failure: %s" s
+  | e -> Options.Self.feedback "exception: %s" (Printexc.to_string e)
 
 let run =
   let deps = [Ast.self; Options.Enabled.self] in
-  Cil.set_useLogicalOperators true;
   let f, _self = State_builder.apply_once "GENLABELS" deps run in
   f
 
