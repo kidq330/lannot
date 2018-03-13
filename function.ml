89399eaec5b87aad40879809c0e799e80acd989d
tmartin <thibault.martin@cea.fr>
2018-03-13 17:00:21 +0100

    Debut Assert

--- function.ml
+++ function.ml
@@ -23,6 +23,7 @@
 open Cil_types
 open Annotators
 open Ast_const
+open Utils
 
 include Register (struct
 
@@ -84,7 +85,7 @@ class visitor = object(_)
 
   method! vstmt_aux stmt =
     match stmt.skind with
-    | Instr i ->
+    | Instr i when not (is_label i) ->
       (match i with
        | Call (_,func,_,_) ->
          (match func.enode with
@@ -294,4 +295,75 @@ module Empty = Annotators.Register (struct
     let apply mk_label file =
       ignore mk_label; (* Avoid warning about mk_label unused *)
       Visitor.visitFramacFileSameGlobals (new nopvisitor :> Visitor.frama_c_visitor) file
+  end)
+
+let unk_loc = Cil_datatype.Location.unknown
+
+let neg atom = Exp.lnot (Exp.copy atom)
+
+let rec mk_expr pred =
+  match pred.pred_content with
+  | Pfalse -> Options.debug "False"; Exp.zero ()
+  | Ptrue  -> Options.debug "True"; Exp.one ()
+  | Pand (p1,p2) -> Options.debug "And"; Exp.binop LAnd (mk_expr p1) (mk_expr p2)
+  | Por (p1,p2)  -> Options.debug "Or"; Exp.binop LOr (mk_expr p1) (mk_expr p2)
+  | Pxor (p1,p2) -> Options.debug "Xor"; Exp.binop BOr (mk_expr p1) (mk_expr p2)
+  | Pnot p -> Options.debug "Not"; Exp.lnot (mk_expr p)
+  | Prel (r,t1,t2) -> Options.debug "Rel";
+    let op = match r with
+      | Rlt -> Lt
+      | Rgt -> Gt
+      | Rle -> Le
+      | Rge -> Ge
+      | Req -> Eq
+      | Rneq -> Ne
+    in
+    Exp.binop op (!Db.Properties.Interp.term_to_exp ~result:None t1) (!Db.Properties.Interp.term_to_exp ~result:None t2)
+  | _ -> Options.feedback "todo" ;Exp.zero ()
+
+
+
+let gen_labels_assert mk_label (pred:Cil_types.predicate) =
+  let loc = pred.pred_loc in
+  let exp = neg (mk_expr pred) in
+  let l1 = mk_label exp [] loc in
+  Stmt.block [l1]
+
+
+
+(** Assert coverage **)
+class visitorAssert mk_label = object(_)
+  inherit Visitor.frama_c_inplace
+
+  val mutable labellist = []
+
+  method! vstmt_aux stmt =
+    ignore stmt;
+    let add_labels stmt =
+      let tmp = labellist in
+      labellist <- [];
+      (Stmt.block (tmp @ [ stmt ]))
+    in
+    Cil.DoChildrenPost add_labels
+
+  method! vcode_annot annot =
+    begin match annot.annot_content with
+      | AAssert (_, p) ->
+        let exp = gen_labels_assert mk_label p in
+        labellist <- labellist @ [exp];
+        Cil.DoChildren
+      | _ ->
+        Cil.DoChildren
+    end
+
+end
+
+(**
+   Assert coverage annotator
+*)
+module AssertCov = Annotators.Register (struct
+    let name = "ASSERT"
+    let help = "Assert Coverage"
+    let apply mk_label file =
+      Visitor.visitFramacFileSameGlobals (new visitorAssert mk_label :> Visitor.frama_c_visitor) file
   end)
---
+++ tests/criteria/test_ASSERT.c
@@ -0,0 +1,12 @@
+/* run.config
+   EXECNOW: LOG @PTEST_NAME@_labels.c @frama-c@ -lannot=CC @PTEST_DIR@/@PTEST_NAME@.c -lannot-debug 1 -lannot-o @PTEST_DIR@/result/@PTEST_NAME@_labels.c > @PTEST_DIR@/result/@PTEST_NAME@_output.log
+ */
+
+
+int main(int a, int b, int c){
+	//@ assert a & b;
+	
+	if (c)
+		return 0;
+	return 1;
+}
