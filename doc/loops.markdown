368b8dd01d7b039a97f28cd3506daf6052471954
tmartin <thibault.martin@cea.fr>
2018-08-07 09:41:19 +0200

    Préparation à la mise en OpenSource (LGPL 3.0)

--- doc/loops.markdown
+++ doc/loops.markdown
@@ -1,3 +1,25 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Frama-C.                                         *)
+(*                                                                        *)
+(*  Copyright (C) 2013-2018                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  You may redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 3.                                                *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful, but WITHOUT     *)
+(*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY    *)
+(*  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General      *)
+(*  Public License for more details.                                      *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 3 for more          *)
+(*  details (enclosed in the file LICENSE).                               *)
+(*                                                                        *)
+(**************************************************************************)
+
 Loops Criteria
 ====================
 
@@ -157,23 +179,3 @@ while (1) {
 ```
 
 As we can see here, without the option the do..while is considered as a normal while, so we execute the body before the label.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- options.ml
+++ options.ml
@@ -2,20 +2,20 @@
 (*                                                                        *)
 (*  This file is part of Frama-C.                                         *)
 (*                                                                        *)
-(*  Copyright (C) 2013-2014                                               *)
+(*  Copyright (C) 2013-2018                                               *)
 (*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
 (*         alternatives)                                                  *)
 (*                                                                        *)
 (*  You may redistribute it and/or modify it under the terms of the GNU   *)
 (*  Lesser General Public License as published by the Free Software       *)
-(*  Foundation, version 2.1.                                              *)
+(*  Foundation, version 3.                                                *)
 (*                                                                        *)
 (*  It is distributed in the hope that it will be useful, but WITHOUT     *)
 (*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY    *)
 (*  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General      *)
 (*  Public License for more details.                                      *)
 (*                                                                        *)
-(*  See the GNU Lesser General Public License version 2.1 for more        *)
+(*  See the GNU Lesser General Public License version 3 for more          *)
 (*  details (enclosed in the file LICENSE).                               *)
 (*                                                                        *)
 (**************************************************************************)
@@ -152,22 +152,21 @@ module LimitDelta = Int (struct
 (* Pour polarSSL *)
 module Inline = True (struct
     let option_name = "-lannot-inline"
-    let help = "Don't ignore inline"
+    let help = "Annotate inline functions (Default : true)"
   end)
 
 module InlineException = Kernel_function_set (struct
     let option_name = "-lannot-inline-functions"
     let arg_name = "funs"
-    let help = "if -lannot-inline is false, then this option permit to add exception and annotate inline functions"
+    let help = "if -lannot-inline is false, then this option allows to add exceptions and annotate some inline functions"
   end)
 
 module MaxContextPath = Int (struct
     let option_name = "-lannot-maxpath"
     let arg_name = "NUM"
-    let help = "set the maximum number of path for one expression with the context criteria (default : 8192)"
+    let help = "set the maximum number of path for one expression with the context criteria (Default : 1024)"
     let default = 1024
   end)
-
 
 (* There is no way to determine the original loop form after the CIL transformation.
    Since loops criterias depend on if we are in a while or do..while.. I added this option to
