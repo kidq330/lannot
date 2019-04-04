73edd1fe49f2852ca7cf47b4f35379e414528b46
Thibault Martin <thibault.martin@cea.fr>
2019-04-04 11:19:49 +0200

    CACC tests following #1 issue

---
+++ tests/criteria/test_CACC.c
@@ -0,0 +1,18 @@
+/* run.config
+   LOG: @PTEST_NAME@_labels.c 
+   LOG: @PTEST_NAME@_labels.labels
+   LOG: @PTEST_NAME@_labels.hyperlabels
+   LOG: @PTEST_NAME@_output.log
+   EXECNOW: @frama-c@ -lannot=CACC @PTEST_DIR@/@PTEST_NAME@.c -lannot-o @PTEST_DIR@/result/@PTEST_NAME@_labels.c > @PTEST_DIR@/result/@PTEST_NAME@_output.log
+   OPT:
+ */
+
+
+int maintest(int a, int b, int c){
+	if (a == b)
+		a = a - b;
+
+	if (a && b || c)
+		return 0;
+	return 1;
+}
