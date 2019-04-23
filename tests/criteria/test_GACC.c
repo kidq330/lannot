2d53d4f806009ac46306e7fc1add1b06e9cc2dae
Thibault Martin <thibault.martin@cea.fr>
2019-04-23 17:42:48 +0200

    Simplification

--- tests/criteria/test_GACC.c
+++ tests/criteria/test_GACC.c
@@ -2,7 +2,7 @@
    LOG: @PTEST_NAME@_labels.c
    LOG: @PTEST_NAME@_labels.labels
    LOG: @PTEST_NAME@_output.log
-   EXECNOW: @frama-c@ -lannot=GACC @PTEST_DIR@/@PTEST_NAME@.c -lannot-o @PTEST_DIR@/result/@PTEST_NAME@_labels.c > @PTEST_DIR@/result/@PTEST_NAME@_output.log
+   EXECNOW: @frama-c@ -lannot=GACC @PTEST_FILE@ -lannot-o @PTEST_DIR@/result/@PTEST_NAME@_labels.c > @PTEST_DIR@/result/@PTEST_NAME@_output.log
    OPT:
  */
 
--- tests/criteria/test_RACC.c
+++ tests/criteria/test_RACC.c
@@ -3,7 +3,7 @@
    LOG: @PTEST_NAME@_labels.labels
    LOG: @PTEST_NAME@_labels.hyperlabels
    LOG: @PTEST_NAME@_output.log
-   EXECNOW: @frama-c@ -lannot=RACC @PTEST_DIR@/@PTEST_NAME@.c -lannot-o @PTEST_DIR@/result/@PTEST_NAME@_labels.c > @PTEST_DIR@/result/@PTEST_NAME@_output.log
+   EXECNOW: @frama-c@ -lannot=RACC @PTEST_FILE@ -lannot-o @PTEST_DIR@/result/@PTEST_NAME@_labels.c > @PTEST_DIR@/result/@PTEST_NAME@_output.log
    OPT:
  */
 
