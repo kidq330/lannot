26672ecd771440af6e18848e0f538e89adbf7434
Virgile Prevosto <virgile.prevosto@m4x.org>
2022-03-22 18:26:17 +0100

    update headers and create Makefile target to automate that

--- bes.ml
+++ bes.ml
@@ -1,3 +1,25 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of the Frama-C's Lannotate plug-in.                 *)
+(*                                                                        *)
+(*  Copyright (C) 2012-2022                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 2.1.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 2.1                 *)
+(*  for more details (enclosed in the file LICENSE)                       *)
+(*                                                                        *)
+(**************************************************************************)
+
 (* $Id: promela.mli 2996 2012-03-14 09:58:12Z weissmam $
 
    Copyright (c) 2011 - 2012 Technische Universitaet Muenchen
